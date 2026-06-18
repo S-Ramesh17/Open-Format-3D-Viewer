7968807436fa1fa2c22b7e8abba771cdc86de46e
Ramesh S <ramesh.sr1706@gmail.com>
2026-06-18 14:56:53 +0530

    fix: use DATABASE_URL_SYNC from environment

--- apps/api/migrations/env.py
+++ apps/api/migrations/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+from app.config import settings
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -8,6 +9,10 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL_SYNC
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
